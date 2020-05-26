f91382cc8aca603d3336ae9ef46d3cd3075af0db
yskeno <yuu.vi4@gmail.com>
2020-05-26 23:28:54 +0900

    Add Chapter2 copies(cannot compile)

---
+++ Chapter03/Game/Game.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "SDL/SDL.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Game
+{
+public:
+	Game();
+	bool Initialize();
+	void RunLoop();
+	void Shutdown();
+
+	void AddActor(class Actor* actor);
+	void RemoveActor(class Actor* actor);
+
+	void AddSprite(class SpriteComponent* sprite);
+	void RemoveSprite(class SpriteComponent* sprite);
+
+	SDL_Texture* GetTexture(const std::string& fileName);
+
+	// ***yskeno***
+	// default = 1024/768
+	float windowW = 800.0f;
+	float windowH = 600.0f;
+
+
+private:
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+	void LoadData();
+	void UnloadData();
+
+	// Map of textures loaded
+	std::unordered_map<std::string, SDL_Texture*> mTextures;
+
+	// All the actors in the game
+	std::vector<class Actor*> mActors;
+	// Any pending actors
+	std::vector<class Actor*> mPendingActors;
+
+	// All the sprite components drawn
+	std::vector<class SpriteComponent*> mSprites;
+
+	SDL_Window* mWindow;
+	SDL_Renderer* mRenderer;
+	Uint32 mTicksCount;
+	bool mIsRunning;
+	// Track if we're updating actors right now
+	bool mUpdatingActors;
+
+	// Game-specific
+	class Ship* mShip;	// Player's ship
+
+	// ***yskeno*** Exercise 2.2
+	class Character* mCharacter;
+};
---
+++ Chapter03/Game/SpriteComponent.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Component.h"
+#include "SDL/SDL.h"
+class SpriteComponent :
+	public Component
+{
+public:
+	// (Lower draw order corresponds with further back)
+	SpriteComponent(class Actor* owner, int drawOrder = 100);
+	~SpriteComponent();
+
+	virtual void Draw(SDL_Renderer* renderer);
+	void Draw(SDL_Renderer* renderer, SDL_RendererFlip rendererFlip);
+	virtual void SetTexture(SDL_Texture* texture);
+
+	int GetDrawOrder() const { return mDrawOrder; }
+	int GetTexWidth() const { return mTexWidth; }
+	int GetTexHeight() const { return mTexHeight; }
+
+private:
+	// Texture to draw
+	SDL_Texture* mTexture;
+	// Draw order used for painter's algorithm
+	int mDrawOrder;
+	// Width/height of texture
+	int mTexWidth;
+	int mTexHeight;
+};
+
