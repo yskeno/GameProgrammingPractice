7198c8e5631e737b9f2782643816bcb62e49a111
yskeno <yuu.vi4@gmail.com>
2020-06-23 22:16:59 +0900

    Add Search.cpp

--- Chapter04/Game/Search.cpp
+++ Chapter04/Game/Search.cpp
@@ -309,3 +309,245 @@ void testHeuristic(bool useASter) {
 	}
 	std::cout << found << '\n';
 }
+
+
+
+/***********
+ Game State
+***********/
+struct GameState {
+	// (For tic-tac-toe, array of board)
+	enum SquareState { Empty, X, O };
+	SquareState mBoard[3][3];
+};
+
+struct GTNode {
+	// Children nodes
+	std::vector<GTNode*> mChildren;
+	// State of game at this node
+	GameState mState;
+};
+
+void GenStates(GTNode* root, bool xPlayer) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (root->mState.mBoard[i][j] == GameState::Empty) {
+				GTNode* node = new GTNode;
+				root->mChildren.emplace_back(node);
+				node->mState.mBoard[i][j] = xPlayer ? GameState::X : GameState::O;
+				GenStates(node, !xPlayer);
+			}
+		}
+	}
+}
+
+float GetScore(const GameState& state) {
+	// Are any of the rows the same?
+	for (int i = 0; i < 3; i++) {
+		bool same = true;
+		GameState::SquareState v = state.mBoard[i][0];
+		for (int j = 1; j < 3; j++) {
+			if (state.mBoard[i][j] != v)
+				same = false;
+		}
+
+		if (same) {
+			if (v == GameState::X)
+				return 1.0f;
+			else
+				return -1.0f;
+		}
+	}
+
+	// Are any of the columns the same
+	for (int j = 0;; j < 3; j++) {
+		bool same = true;
+		GameState::SquareState v = state.mBoard[0][j];
+		for (int i = 1; i < 3; i++) {
+			if (state.mBoard[i][j] != v)
+				same = false;
+		}
+
+		if (same) {
+			if (v == GameState::X)
+				return 1.0f;
+			else
+				return -1.0f;
+		}
+	}
+
+	// What about diagonals?
+	if (((state.mBoard[0][0] == state.mBoard[1][1]) &&
+		(state.mBoard[1][1] == state.mBoard[2][2])) ||
+		((state.mBoard[2][0] == state.mBoard[1][1]) &&
+			(state.mBoard[1][1] == state.mBoard[0][2]))) {
+		if (state.mBoard[1][1] == GameState::X)
+			return 1.0f;
+		else
+			return -1.0f;
+	}
+	// We tied
+	return 0.0f;
+}
+
+float MinPlayer(const GTNode* node);
+
+// Returns the best possible score.
+// But this doesn't specify which next move is optimal.
+float MaxPlayer(const GTNode* node) {
+	// If this is a leaf, return score
+	if (node->mChildren.empty())
+		return GetScore(node->mState);
+
+	// Find the subtree with the maximum value
+	float maxValue = -std::numeric_limits<float>::infinity();
+	for (const GTNode* child : node->mChildren) {
+		maxValue = std::max(maxValue, MinPlayer(child));
+	}
+	return maxValue;
+}
+
+// Return the subtree with the lowest score.
+float MinPlayer(const GTNode* node) {
+	// If this is a leaf, return score
+	if (node->mChildren.empty())
+		return GetScore(node->mState);
+
+	float minValue = std::numeric_limits<float>::infinity();
+	// Find the subtree with the minimum value
+	for (const GTNode* child : node->mChildren) {
+		minValue = std::min(minValue, MaxPlayer(child));
+	}
+	return minValue;
+}
+
+// Determine the best move.
+const GTNode* MinimaxDecide(const GTNode* root) {
+	// Find the subtree with the maximum value, and save the choice
+	const GTNode* choice = nullptr;
+	float maxValue = -std::numeric_limits<float>::infinity();
+	for (const GTNode* child : root->mChildren) {
+		float v = MinPlayer(child);
+		if (v > maxValue) {
+			maxValue = v;
+			choice = child;
+		}
+	}
+	return choice;
+}
+
+/*****************************
+ Alpha-Beta Pruning from Book
+*****************************/
+/*
+const GameState* AlphaBetaDecide(const GameState* root, int maxDepth) {
+	const GameState* choice = nullptr;
+	// Alpha starts at negative infinity, beta at positive infinity
+	float maxValue = -std::numeric_limits<float>::infinity();
+	float beta = std::numeric_limits<float>::infinity();
+	for (const GameState* child : root->GetPossibleMoves()) {
+		float v = AlphaBetaMin(child, maxDepth - 1, beta);
+		if (v > maxValue) {
+			maxValue = v;
+			choice = child;
+		}
+	}
+	return choice;
+}
+
+float AlphaBetaMax(const GameState* node, int depth, float alpha, float beta) {
+	if (depth == 0 || node->IsTerminal())
+		return node->GetScore();
+
+	float maxValue = -std::numeric_limits<float>::infinity();
+	for (const GameState* child : node->GetPossibleMoves()) {
+		maxValue = std::max(maxValue, AlphaBetaMin(child, depth - 1, alpha, beta));
+		if (maxValue >= beta)
+			return maxValue;	// Beta prune
+		alpha = std::max(maxValue, alpha);	// Increase lower bound
+	}
+	return maxValue;
+}
+
+float AlphaBetaMin(const GameState* node, int depth, float alpha, float beta) {
+	if (depth == 0 || node->IsTerminal())
+		return node->GetScore();
+	float minValue = std::numeric_limits<float>::infinity();
+	for (const GameState* child : node->GetPossibleMoves()) {
+		minValue = std::min(minValue, AlphaBetaMax(child, depth - 1, alpha, beta));
+		if (minValue <= alpha)
+			return minValue;	// Alpha prune
+		beta = std::min(minValue, beta);	// Decrease upper bound
+	}
+	return minValue;
+}
+*/
+
+/***********************************
+ Alpha-Beta Pruning from Repository
+***********************************/
+float AlphaBetaMin(const GTNode* node, float alphe, float beta);
+
+float AlphaBetaMax(const GTNode* node, float alpha, float beta) {
+	// If this is a leaf, return score
+	if (node->mChildren.empty())
+		return GetScore(node->mState);
+
+	float maxValue = -std::numeric_limits<float>::infinity();
+	// Find the subtree with the maximum value
+	for (const GTNode* child : node->mChildren) {
+		maxValue = std::max(maxValue, AlphaBetaMin(child, alpha, beta));
+		if (maxValue >= beta)
+			return maxValue;	// Beta prune
+		alpha = std::max(maxValue, alpha);
+	}
+	return maxValue;
+}
+
+float AlphaBetaMin(const GTNode* node, float alpha, float beta) {
+	// If this is a leaf, return score
+	if (node->mChildren.empty())
+		return GetScore(node->mState);
+
+	float minValue = std::numeric_limits<float>::infinity();
+	// Find the subtree with the minimum value
+	for (const GTNode* child : node->mChildren) {
+		minValue = std::min(minValue, AlphaBetaMax(child, alpha, beta));
+		if (minValue <= alpha)
+			return minValue;	// Alpha prune
+		beta = std::min(minValue, beta);
+	}
+	return minValue;
+}
+
+const GTNode* AlphaBetaDecide(const GTNode* root) {
+	// Find the subtree with the maximum value, and save the choice
+	const GTNode* choice = nullptr;
+	float maxValue = -std::numeric_limits<float>::infinity();
+	float beta = std::numeric_limits<float>::infinity();
+	for (const GTNode* child : root->mChildren) {
+		float v = AlphaBetaMin(child, maxValue, beta);
+		if (v > maxValue) {
+			maxValue = v;
+			choice = child;
+		}
+	}
+	return choice;
+}
+
+void testTicTac() {
+	GTNode* root = new GTNode;
+	root->mState.mBoard[0][0] = GameState::O;
+	root->mState.mBoard[0][1] = GameState::Empty;
+	root->mState.mBoard[0][2] = GameState::X;
+	root->mState.mBoard[1][0] = GameState::X;
+	root->mState.mBoard[1][1] = GameState::O;
+	root->mState.mBoard[1][2] = GameState::O;
+	root->mState.mBoard[2][0] = GameState::X;
+	root->mState.mBoard[2][1] = GameState::Empty;
+	root->mState.mBoard[2][2] = GameState::Empty;
+
+	GenStates(root, true);
+	const GTNode* choice = AlphaBetaDecide(root);
+	std::cout << choice->mChildren.size();
+}
